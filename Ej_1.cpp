eaae389245836c0673515ebe3c66fd0a8ff52775
icalviello <calviellonacho@gmail.com>
2024-09-10 15:41:37 -0300

    agrega validaciones

--- Ej_1.cpp
+++ Ej_1.cpp
@@ -8,10 +8,18 @@ int main()
     cout << " ingrese un numero " <<endl;
     cin >> numero;
 
-    for(int i = 1; i <= 10; i++)
+    if(numero <= 0)
+    {
+        cout << "ingrese un numero mayor a 0 ";
+    }
+    else{
+        for(int i = 1; i <= 10; i++)
     {
         cout << numero << " x " << i << " = " << numero*i << endl;
     }
+
+    }
+
 
 
 
